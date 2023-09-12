2adc356f3ac3b8dc3c64acf533cb7f51ce47af8a
Jas Dziedziak <jas.dzied@gmail.com>
2023-09-12 16:28:16 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -27,8 +27,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-        test
-        t
+        test added a short message
       </p>
     </>
   )
